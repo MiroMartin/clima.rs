aa218869438a8c94d53be8eb0e101f6d88f8a632
MiroMartin <mmiro578@gmail.com>
2025-09-30 00:39:50 -0300

    agregar una variable

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use clima::clima_json_request;
+use std::process;
 
 pub mod clima;
 
 fn main() {
     let url = "https://api.open-meteo.com/v1/forecast?latitude=-36.3569&longitude=-56.7235&current=temperature_2m,apparent_temperature&forecast_days=1";
 
-    let _ = clima_json_request(url);
+    if let Err(e) = clima_json_request(url) {
+        process::exit(1);
+    }
 }
